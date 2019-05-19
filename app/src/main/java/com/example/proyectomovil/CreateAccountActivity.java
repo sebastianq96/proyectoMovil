5d76fba470c80ce1ab2be17112a713d9fb04e772
sebastianq96 <sebastian.quintero_o@uao.edu.co>
2019-05-19 15:36:46 -0500

    Subiendo proyecto ahora si xD

---
+++ app/src/main/java/com/example/proyectomovil/CreateAccountActivity.java
@@ -0,0 +1,19 @@
+package com.example.proyectomovil;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toolbar;
+
+public class CreateAccountActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+
+    }
+
+
+}
