32d2e6fa842d5fe02962df45bd4487e5f0ddc386
cristianreyes214 <cristian.reyes@uao.edu.co>
2019-05-19 01:39:59 -0500

    se me olvido este en el anterior commit

--- app/src/main/java/com/example/proyectomovil/MainActivity.java
+++ app/src/main/java/com/example/proyectomovil/MainActivity.java
@@ -9,6 +9,7 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
 
     private Button boton1;
+    private Button boton2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,12 +17,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         boton1 = (Button)findViewById(R.id.boton1);
+        boton2 = (Button)findViewById(R.id.boton2);
 
         boton1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, ActivityProfesor.class);
-                startActivity(intent);
+                Intent intent1 = new Intent(MainActivity.this, ActivityProfesor.class);
+                startActivity(intent1);
+            }
+        });
+        boton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent2 = new Intent(MainActivity.this, ActivityEstudiantes.class);
+                startActivity(intent2);
             }
         });
     }
