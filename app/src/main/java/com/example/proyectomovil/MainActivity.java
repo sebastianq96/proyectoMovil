d06435313f8be18dc66f27dc776dd9ed73e41767
cristianreyes214 <cristian.reyes@uao.edu.co>
2019-05-19 00:37:54 -0500

    agrego menu hamburguesa de profes

--- app/src/main/java/com/example/proyectomovil/MainActivity.java
+++ app/src/main/java/com/example/proyectomovil/MainActivity.java
@@ -1,13 +1,28 @@
 package com.example.proyectomovil;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button boton1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        boton1 = (Button)findViewById(R.id.boton1);
+
+        boton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ActivityProfesor.class);
+                startActivity(intent);
+            }
+        });
     }
 }
