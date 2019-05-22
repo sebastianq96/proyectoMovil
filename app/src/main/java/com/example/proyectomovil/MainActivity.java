a8c7040bec9a648e48af6314e0c2c333e8e6bb5b
cristianreyes214 <cristian.reyes@uao.edu.co>
2019-05-21 19:42:18 -0500

    falto esto jeje

--- app/src/main/java/com/example/proyectomovil/MainActivity.java
+++ app/src/main/java/com/example/proyectomovil/MainActivity.java
@@ -4,16 +4,39 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button bEntrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bEntrar = (Button)findViewById(R.id.entrar);
 
+        //FUNCIÓN, LUEGO DEL LOGGIN QUE ENVIARÁ
+        bEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                /*String log, pass;
+                log = textUsuario.getText().toString();
+                pass = textContrasena.getText().toString();
 
+                if(log.equals("admin") && pass.equals("12345")){
+                 */   //Pasar a siguiente interfaz
+                    Intent intent = new Intent(MainActivity.this, perfilActivity.class);
+                    startActivity(intent);
+                /*}
+                else {
+                    textUsuario.setText("");
+                    textContrasena.setText("");
+                }*/
+            }
+        });
     }
+
 
     public void goCreateAccount(View view){
         Intent intent = new Intent(this, CreateAccountActivity.class);
